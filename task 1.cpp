c3ffb1d03062647e60a0160813cfa806377b2550
Shyam841501 <shyamkumarmishra1234@gmail.com>
2025-05-04 02:29:31 +0530

    first commit

--- task 1.cpp
+++ task 1.cpp
@@ -2,6 +2,7 @@
 #include<iostream>
 using namespace std;
 void bubbleSort(int arr[],int n){
+    
     for(int i=0;i<n;i++){
         for(int j=0;j<n-i;j++){
             if(arr[j]>arr[j+1]){ //compair
--- task 2.cpp
+++ task 2.cpp
@@ -1,1 +1,31 @@
 // insertion sort
+// linear search
+#include <iostream>
+using namespace std;
+int linearSearch(int arr[], int n)
+{
+    int a;
+    cout << "What you have to find :";
+    cin >> a;
+    for (int i = 0; i < n; i++)
+    {   
+        if (a == arr[i])
+        {
+            return i;//return the indenx of matching element
+        }
+    }
+    return -1; //not found
+}
+int main()
+{
+
+    int arr[7];
+
+    for (int i = 0; i < 7; i++)
+    {
+        cout << "Enter index no.:" << i << " ";
+        cin >> arr[i];
+    }
+    int a = linearSearch(arr, 7);
+    cout << a << " is the position of number";
+}
